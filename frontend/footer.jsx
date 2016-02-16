155905530cf1c20a85c2996570770db895174421
Hank Fanchiu <hank@fanchiu.com>
2016-02-16 01:25:23 -0800

    Remove Contact; add News to footer

--- frontend/footer.jsx
+++ frontend/footer.jsx
@@ -1,5 +1,5 @@
 var React = require("react");
-var browserHistory = require('react-router').browserHistory;
+var browserHistory = require("react-router").browserHistory;
 
 var Footer = React.createClass({
   pushToHome: function () {
@@ -13,15 +13,16 @@ var Footer = React.createClass({
   render: function () {
     return (
       <footer className="footer">
-
         <p className="footer-links">
             <a onClick={this.pushToHome}>Home</a>
             |
-            <a href="https://github.com/hankfanchiu/package-tracker" target="_blank">Github</a>
-            |
             <a onClick={this.pushToAbout}>About</a>
             |
-            <a href="mailto:fanchiu.hank@gmail.com">Contact</a>
+            <a href="https://github.com/hankfanchiu/trackmonkey"
+              target="_blank">GitHub</a>
+            |
+            <a href="https://twitter.com/goshippo/status/699044208267517952"
+              target="_blank">News</a>
         </p>
 
         <p className="footer-company-name">TrackMonkey &copy; 2016</p>
