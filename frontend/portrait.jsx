56439c63102b22764afc17c53c5d36998fdc4748
Hank Fanchiu <hank@fanchiu.com>
2016-02-16 01:16:55 -0800

    Refactor About and Portrait components

--- frontend/portrait.jsx
+++ frontend/portrait.jsx
@@ -1,17 +1,19 @@
 var React = require("react");
 
 var Portrait = React.createClass({
-
   render: function () {
-    var portraitDetails = this.props.portraitDetails;
+    var portrait = this.props.portrait;
+
     return (
-      <div>
-        <a href={portraitDetails["linkedin"]} target="_blank"><img src={portraitDetails["img"]}></img></a>
-        <br/>
-        {portraitDetails["name"]}
-        <br/>
-        {portraitDetails["role"]}
-      </div>
+      <figure>
+        <a href={portrait.linkedin} target="_blank">
+          <img src={portrait.img}></img>
+        </a>
+        <p>
+          {portrait.name}<br/>
+          {portrait.role}
+        </p>
+      </figure>
     );
   }
 });
