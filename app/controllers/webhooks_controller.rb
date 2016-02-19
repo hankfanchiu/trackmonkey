ef96f3bace1874d3633390bc5d938f66a229e617
Hank Fanchiu <fanchiu.hank@gmail.com>
2016-02-19 00:38:16 -0800

    Add safety checks to 'webhooks#create'

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,18 +4,27 @@ class WebhooksController < ApplicationController
   def create
     tracking_number = params[:tracking_number]
     tracking_status = params[:tracking_status]
-    carrier = params[:carrier]
 
-    if tracking_number && tracking_status != "null"
-      Package.send_batch_updates(
-        tracking_number,
-        tracking_status[:status],
-        carrier
-      )
+    if tracking_number && !tracking_status.nil?
+      send_batch_updates
 
       render json: {}, status: :ok
     else
       render json: {}, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def send_batch_updates
+    tracking_number = params[:tracking_number]
+    tracking_status = params[:tracking_status]
+    carrier = params[:carrier]
+
+    return unless tracking_status.is_a?(Hash)
+
+    status = tracking_status[:status]
+
+    Package.send_batch_updates(tracking_number, status, carrier)
   end
 end
