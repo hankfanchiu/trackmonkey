6b989f04ffdda3323675742ec3d500553a670fbb
Hank Fanchiu <fanchiu.hank@gmail.com>
2016-02-22 21:40:23 -0800

    Space out React routes

--- frontend/routes.jsx
+++ frontend/routes.jsx
@@ -1,6 +1,7 @@
 var React = require("react");
 var Route = require("react-router").Route;
 var IndexRoute = require("react-router").IndexRoute;
+
 var App = require("./components/app");
 var Home = require("./components/home/home");
 var Tracking = require("./components/tracking/tracking");
@@ -9,6 +10,7 @@ var About = require("./components/about/about");
 var routes = (
 	<Route path="/" component={App}>
 		<IndexRoute component={Home}/>
+
 		<Route path="tracking/:shipment" component={Tracking}/>
 		<Route path="about" component={About}/>
 	</Route>
